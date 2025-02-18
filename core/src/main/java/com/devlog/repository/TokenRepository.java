4d151acd6082bbde27f744b0ef62fd444ecb24b5
JeYeongR <wpdud2003@gmail.com>
2025-02-18 23:50:50 +0900

    [V1] refactor: 토큰 래포지토리 삭제

--- core/src/main/java/com/devlog/repository/TokenRepository.java
+++
@@ -1,8 +0,0 @@
-package com.devlog.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.devlog.domain.Token;
-
-public interface TokenRepository extends JpaRepository<Token, Long> {
-}
--- core/src/main/java/com/devlog/service/TokenIssueService.java
+++ core/src/main/java/com/devlog/service/TokenIssueService.java
@@ -3,9 +3,7 @@ package com.devlog.service;
 import org.springframework.stereotype.Service;
 
 import com.devlog.domain.Token;
-import com.devlog.domain.User;
 import com.devlog.external.security.JwtProvider;
-import com.devlog.repository.TokenRepository;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,13 +13,12 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 public class TokenIssueService {
 
-	private final TokenRepository tokenRepository;
 	private final JwtProvider jwtProvider;
 
 	public Token createTokens(Long userId) {
 		String accessToken = jwtProvider.createAccessToken(userId);
 		String refreshToken = jwtProvider.createRefreshToken(userId);
 
-		return tokenRepository.save(Token.create(accessToken, refreshToken));
+		return Token.create(accessToken, refreshToken);
 	}
 }
